2b0a5eacbb4fa6fc71a7908e6d4cd08b9957fe86
Johanbo22 <jbk@famkjaer.dk>
2025-01-06 16:07:12 +0100

    Tilføjet grouped_data.to_excel()

--- plots_of_lab_results.py
+++ plots_of_lab_results.py
@@ -104,7 +104,7 @@ grouped_data = data.groupby(["Dybde", "Lokation"]).agg(
     Uorganisk_fosfor_se = ("Uorganisk fosfor (kg/ha)", standard_error)
 ).reset_index() # resetter index-værdierne i kolonnerne til [0, 1, 2 osv] fremfor dybde. 
 
-grouped_data.to_json('groupeddata_output.json', orient="columns")
+grouped_data.to_excel('groupeddata_output.excel')
 
 # Funktion til at oprette plots
 def plot_metric_with_depth(data, metric_mean, metric_se, title, xlabel):
