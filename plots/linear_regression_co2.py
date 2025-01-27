42df35bfc54e53b309176f6c28dfad453b4010ee
Johanbo22 <jbk@famkjaer.dk>
2025-01-27 22:10:27 +0100

    Update linear_regression_co2.py

--- plots/linear_regression_co2.py
+++ plots/linear_regression_co2.py
@@ -1,7 +1,9 @@
+from typing import MutableMapping
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 from scipy.stats import linregress
+from matplotlib.ticker import MultipleLocator
 
 
 # Data
@@ -16,7 +18,7 @@ data_co2 = pd.DataFrame({
 def plot_linear_regression(data, x_columns, y_columns, title, xlabel, ylabel):
     plt.rcParams['font.family'] = 'DeJavu Serif'
     plt.rcParams["font.serif"] = "Times New Roman"
-    plt.figure(figsize=(10, 6), facecolor="#FFFFFF")
+    plt.figure(figsize=(8, 4), facecolor="#FFFFFF")
 
     for column in y_columns:
         x = data[x_columns]
@@ -26,42 +28,44 @@ def plot_linear_regression(data, x_columns, y_columns, title, xlabel, ylabel):
         line = slope * x + intercept
         r_squared = r_value**2
 
-        plt.scatter(x, y, color="darkorange")
+        plt.scatter(x, y, color="grey")
         plt.plot(x, line, color="black", label=f"y = {slope:.4f}x + {intercept:.2f}\n" f"R² = {r_squared:.4f}")
 
 
-    plt.title(title, fontsize=12, pad=5, fontweight="bold") # formattering
+    #plt.title(title, fontsize=12, pad=5, fontweight="bold") # formattering
     
-    plt.xlabel(xlabel, fontsize=8, fontweight="bold")
+    plt.xlabel(xlabel, fontsize=10)
     plt.xticks(np.arange(0, 301, 50)) # formattering
-    plt.ylabel(ylabel, fontsize=8, fontweight="bold")
-    plt.gca().set_facecolor("#F0F0F0") # farven af akse-displayet
-    plt.gca().tick_params(direction="in", which="both")
+    ax = plt.gca()
+    ax.xaxis.set_minor_locator(MultipleLocator(10))
+    plt.ylabel(ylabel, fontsize=10)
+    plt.gca().set_facecolor("white") # farven af akse-displayet
+    plt.gca().tick_params(direction="out", which="both")
     plt.subplots_adjust(right=0.75)
     legend = plt.legend(
-        bbox_to_anchor=(0.5, -0.1),
-        loc="upper center",
-        fontsize=7,
+        #bbox_to_anchor=(0.5, -0.1)
+        loc="best",
+        fontsize=12,
         title_fontsize=8,
-        frameon=True,
-        facecolor="#F0F0F0",
+        frameon=False,
+        facecolor="white",
         edgecolor="black",
-        shadow=True,
-        fancybox=True,
+        shadow=False,
+        fancybox=False,
         borderpad=1,
-        labelspacing=1.5,
+        labelspacing=1.0,
         handlelength=2,
-        handleheight=1.5,
+        handleheight=1,
         ncol=1 # antal kolonner til legend
     )
-    plt.grid(axis="both", linestyle="--", alpha=0.1, color="#000000")
+    #plt.grid(axis="both", linestyle="--", alpha=0.1, color="#000000")
 
     for spine in plt.gca().spines.values(): # formattering af kanten på akserne
         spine.set_visible(True)
-        spine.set_linewidth(2)
+        spine.set_linewidth(0.5)
         spine.set_edgecolor("black")
 
     plt.tight_layout(rect=[0, 0, 0.75, 1]) # formattering
     plt.show() 
                  
-plot_linear_regression(data_co2, "Tid (sek)", ["0-5cm"], "CO₂ produktion for dybden 0-5cm", "Tid (sek)", "CO₂ (µg CO₂-C t⁻¹)")
+plot_linear_regression(data_co2, "Tid (sek)", ["0-5cm"], "CO₂ produktion for dybden 0-5cm", "Sekunder", "CO₂ (µg CO₂-C g⁻¹ t⁻¹)")
