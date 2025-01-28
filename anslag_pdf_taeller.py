531397a001a759e0c368035cbf49f590cb494cac
Johanbo22 <jbk@famkjaer.dk>
2025-01-28 14:24:30 +0100

    Update anslag_pdf_taeller.py

--- anslag_pdf_taeller.py
+++ anslag_pdf_taeller.py
@@ -5,39 +5,55 @@ def pdf_anslag(pdf_path, startside, slutside):
         with open(pdf_path, 'rb') as pdf_file:
             reader = PyPDF2.PdfReader(pdf_file)
 
-            # tom streng
             text= ""
 
             # indlæser sidetalet
             for page_number in range(startside - 1, slutside):
                 if page_number < len(reader.pages):
-                    text += reader.pages[page_number].extract_text() or ""
-                else:
-                    print(f"Advarsel: Sidetallet {page_number + 1} findes ikk i PDF")
+                    page_text = ""
+
+                    try:
+                        page_text = reader.pages[page_number].extract_text() or ""
+                        if page_text.strip():
+                            text += page_text
+                        else:
+                            print(f"Side {page_number + 1} indeholder ikke tekst")
+                    except Exception as e:
+                        print(f"Kunne ikke læse side {page_number + 1}. Fejl {e}")
+                else: 
+                    print(f"Sidetall {page_number + 1} findes ikke")
             
-            # antallet af tegn
             character_count = len(text)
 
             return character_count
     
-    except Exception as e:
-        print(f" Der er sket en fejl: {e}")
+    except FileNotFoundError:
+        print(f"Fejl: pdf-fil ikke fundet på denne sti")
         return None
-    
+    except Exception as e:
+        print(f"Der er sket en fejl: {e}")
+        return None
 
 if __name__ == "__main__":
-    pdf_path = r"" # stien til pdf-filen
-    try: 
-        startside = int(input("indtast startside: "))
-        slutside = int(input("indtast slutside: "))
+    pdf_path = r"C:\Users\joha4\OneDrive\Skrivebord_LapTop\Bachelorprojekt\Python\Rapport_til_geolab.pdf"
+    try:
+        startside = int(input("Indtast startside: "))
+        slutside = int(input("Indtast slutside: "))
 
-        character_count = pdf_anslag(pdf_path, startside, slutside) # antallet af tegn fra funktionen
-        antal_sider = character_count / 2400 # for at få antal normalsider
+        if startside <= 0 or slutside <= 0:
+            print(f"Startside og slutside skal være større end 0")
+        elif startside > slutside:
+            print("Startsiden skal være  mindre el lig md slutside")
+        else:
+            character_count = pdf_anslag(pdf_path, startside, slutside)
 
-        if character_count is not None:
-            print("-------------------------------------------------------------------")
-            print(f"Antal anslag i Pdf fra side {startside} til {slutside} er: {character_count}")
-            print(f"Antallet af sider: {antal_sider}")
-            print("-------------------------------------------------------------------")
+            if character_count is not None:
+                antal_sider = character_count / 2400
+                print("-------------------------------------------------------------------")
+                print(f"Antal anslag i PDF fra side {startside} til {slutside} er: {character_count}")
+                print(f"Antallet af sider (afrundet): {antal_sider:.2f} normalsider")
+                print("-------------------------------------------------------------------")
     except ValueError:
-        print(f"indtast korrekte sidetal")
+        print("Fejl: Indtast venligst gyldige heltal for start- og slutsider.")
+
+                
